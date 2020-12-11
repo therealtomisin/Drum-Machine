6fc4966099cfa0ff6f14430c156246b8b56a8834
Agboolaidris <agboolaisholaidreez@gmail.com>
2020-12-11 12:22:13 +0100

    update file by idris agboola

--- drum-machine/src/App.js
+++ drum-machine/src/App.js
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react'
+import React, {useRef, useState} from 'react'
 import './App.css';
 import Drumbody from './Components/DrumBody'
 
@@ -6,46 +6,62 @@ function App() {
   const sounds = [
     {
       id: 'A',
-      aud: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+      aud: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+      name:'aaa'
     },
     {
       id: 'C',
-      aud: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+      aud: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
+      name:'ccc'
     },
     {
       id: 'D',
-      aud: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+      aud: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+      name:'ddd'
     },
     {
       id: 'E',
-      aud: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+      aud: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
+      name:'eee'
     },
     {
       id: 'Q',
-      aud: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+      aud: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
+      name:'qqq'
     },
     {
       id: 'S',
-      aud: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+      aud: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
+      name:'sss',
     },
     {
       id: 'W',
-      aud: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+      aud: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+      name:'www'
     },
     {
       id: 'X',
-      aud: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+      aud: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+      name:'xxx'
     },
     {
       id: 'Z',
-      aud: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+      aud: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
+      name:'zzz'
     }
   ]
 
+  const [display, setdisplay] = useState('')
+
+  const callback = (name)=>{
+    setdisplay(name)
+  }
+
   return (
     <div id = "drum-machine" className="App">
+      <div id='display'>{display}</div>
     <div className = 'drum-body'>
-      <Drumbody sounds = {sounds} sound = {sounds.aud} />
+      <Drumbody sounds = {sounds} sound = {sounds.aud}  callback={callback}/>
     </div>
     </div>
   );
--- drum-machine/src/Components/DrumBody.js
+++ drum-machine/src/Components/DrumBody.js
@@ -4,16 +4,17 @@ function DrumBody(props) {
 
     const soundRef = useRef(null)
 
-    const play = () => {
+    const play = (name) => {
         console.log(soundRef.current)
         soundRef.current.play()
+        props.callback(name)
     }
 
     return (
         <div className = 'display' id = 'display'>
             {props.sounds.map((sound, index)=>{
                 return (
-                        <button key = {index} className = 'drum-pad' onClick = {play}>
+                        <button key = {index} className = 'drum-pad' onClick = {()=>play(sound.name)}>
                             <audio ref = {soundRef} src = {sound.aud}/>
                             {sound.id}
                         </button>
